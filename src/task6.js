b40478c2d6682b13131490d873febf7d172fb995
FriendSK <49130sk@gmail.com>
2019-04-25 01:52:59 +0300

    'HomeWork'

--- src/task6.js
+++ src/task6.js
@@ -3,18 +3,18 @@ jsElementaryTasks.task6 = function (row, min) {
     function getSequence(row, min) {
 
         let resultArr = [];
-        let len = transformFloatData(row);
-        let square = transformFloatData(min);
-        let sqrt = +Math.sqrt(square).toFixed(0);
+        let len = transformFloatData(row);     // parseFloat incoming Data
+        let square = transformFloatData(min);    // parseFloat incoming Data
+        let sqrt = +Math.sqrt(square).toFixed(0);  //calculating square root
 
-        if (!validationDataForSequence(len, sqrt)) {
+        if (!validationDataForSequence(len, sqrt)) {  //validation of the incoming data
 
-            for (let i = sqrt; i < len + sqrt; i++) {
-                resultArr.push(i);
+            for (let i = sqrt; i < sqrt + len; i++) {  //loop from square root to sum of  square root and lenght 
+                resultArr.push(i);                      //push the suitable  numbers to array
             }
-            return resultArr.join(',');
+            return resultArr.join(',');                 // make a string from array
 
-        } else return `Status: ${obj.status}, reason: ${obj.reason}`;
+        } else return `Status: ${obj.status}, reason: ${obj.reason}`; //if an error, return the status
     };
     return getSequence(row, min);
 };
--- src/task7.js
+++ src/task7.js
@@ -1,51 +1,51 @@
 jsElementaryTasks.task7 = function (event, context) {
 
     function getFiboByLength(context) {
-        let len = transformIntData(context.length);
+        let len = transformIntData(context.length); // parseInt incoming Data
         let getNum = 0;
         let arr = [0, 1];
         let res = [];
 
-        if (!validationDataForFiboByLength(len)) {
+        if (!validationDataForFiboByLength(len)) {  //validation of the incoming data
 
-            for (let i = 2; getNum.toString().length < len + 1; i++) {
+            for (let i = 2; getNum.toString().length < len + 1; i++) { //loop from  last but one element length to a given
 
-                arr.push(arr[i - 1] + arr[i - 2]);
-                getNum = arr[i - 1];
-                if (getNum.toString().length === len) {
-                    res.push(getNum);
+                arr.push(arr[i - 1] + arr[i - 2]);          // push the sum of the last two elements into origin array [0, 1]
+                getNum = arr[i - 1];                        //get the last but one element of arr
+                if (getNum.toString().length === len) {   // comparison of its  string length to a given length and pushing 
+                    res.push(getNum);                      //numbers that match to a given length
                 }
             };
             return res;
-        } else return `Status: ${obj.status}, reason: ${obj.reason}`;
+        } else return `Status: ${obj.status}, reason: ${obj.reason}`;  //if an error, return the status
     };
 
     function getFiboByRange(context) {
 
-        let min = transformFloatData(context.min);
-        let max = transformFloatData(context.max);
+        let min = transformFloatData(context.min);   // parseFloat incoming Data
+        let max = transformFloatData(context.max);    // parseFloat incoming Data
         let getNum = 0;
         let arr = [0, 1];
         let res = [];
 
-        if (!validationDataForFiboByRange(+context.min, +context.max)) {
+        if (!validationDataForFiboByRange(+context.min, +context.max)) {  //validation of the incoming data
 
-            for (let i = 2; getNum < max; i++) {
+            for (let i = 2; getNum < max; i++) {        ////loop from  last but one element to a given max length
 
-                arr.push(arr[i - 1] + arr[i - 2]);
-                getNum = arr[i - 1];
-                if (getNum >= min && getNum <= max) {
+                arr.push(arr[i - 1] + arr[i - 2]);       // push the sum of the last two elements into origin array [0, 1]
+                getNum = arr[i - 1];                    //get the last but one element of arr 
+                if (getNum >= min && getNum <= max) {    //comparison and pushing current element if it match to a given interval
                     res.push(getNum);
                 }
             };
             return res;
-        } else return `Status: ${obj.status}, reason: ${obj.reason}`;
+        } else return `Status: ${obj.status}, reason: ${obj.reason}`;  //if an error, return the status
     };
 
     function getAnswerChoise(event) {
 
-        if (event.target.getAttribute('data') === '2')
-
+        if (event.target.getAttribute('data') === '2')  // get the choise of calculations depending on 
+                                                        // the  pressed button
             return getFiboByLength(context);
 
         else return getFiboByRange(context);
